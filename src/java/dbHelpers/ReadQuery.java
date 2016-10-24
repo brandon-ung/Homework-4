2f25d942fbce1d319456d2998f1e6120e6d8c3fc
Brandon Ung <brandon-ung@uiowa.edu>
2016-10-23 21:55:19 -0500

    Added ReadQuery class in dbHelpers package to set up db connection and read data into a table

---
+++ src/java/dbHelpers/ReadQuery.java
@@ -0,0 +1,106 @@
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Athletes;
+
+public class ReadQuery {
+    private Connection conn;
+    private ResultSet results;
+    
+    public ReadQuery(){
+    Properties props = new Properties();
+    InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    String driver = props.getProperty("driver.name");
+    String url = props.getProperty("server.name");
+    String username = props.getProperty("user.name");
+    String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ }
+    
+    public void doRead(){
+        try {
+            String query = "Select * from athletes";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String getHTMLtable(){
+        String table = "";
+        
+        table += "<table border=1>";
+        
+        try {
+            while(this.results.next()){
+                Athletes athletes = new Athletes();
+                athletes.setAthleteID(this.results.getInt("athleteID"));
+                athletes.setAthleteName(this.results.getString("athleteName"));
+                athletes.setSport(this.results.getString("sport"));
+                athletes.setNationality(this.results.getString("nationality"));
+                athletes.setAge(this.results.getInt("age"));
+                
+                table += "<tr>";
+                
+                table += "<td>";
+                table += athletes.getAthleteID();
+                table += "</td>";
+                
+                table += "<td>";
+                table += athletes.getAthleteName();
+                table += "</td>";
+                
+                table += "<td>";
+                table += athletes.getSport();
+                table += "</td>";
+                
+                table += "<td>";
+                table += athletes.getNationality();
+                table += "</td>";
+                
+                table += "<td>";
+                table += athletes.getAge();
+                table += "</td>";
+                
+                table += "</tr>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        table += "</table>";
+        
+        return table;
+    }
+}
