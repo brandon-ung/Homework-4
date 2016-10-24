2db41971e8149ce8de75e0b577b872d28504c40c
Brandon Ung <brandon-ung@uiowa.edu>
2016-10-23 21:16:50 -0500

    Athletes class complete with getters, setters, and toString method

--- src/java/model/Athletes.java
+++ src/java/model/Athletes.java
@@ -23,5 +23,49 @@ public class Athletes {
         this.age = age;
     }
     
+        public int getAthleteID() {
+        return athleteID;
+    }
+
+    public void setAthleteID(int athleteID) {
+        this.athleteID = athleteID;
+    }
+
+    public String getAthleteName() {
+        return athleteName;
+    }
+
+    public void setAthleteName(String athleteName) {
+        this.athleteName = athleteName;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Athletes{" + "athleteID=" + athleteID + ", athleteName=" + athleteName + ", sport=" + sport + ", nationality=" + nationality + ", age=" + age + '}';
+    }
     
 }
